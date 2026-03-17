a7c27bcb916c9d6bee57b20c19ad29e9b984605f
KahramanBerkeCelenay <s28379@pjwstk.edu.pl>
2026-03-17 23:14:07 +0100

    Add sample integer array for future calculations

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-public static void SayHello() { Console.WriteLine("Hello"); }
+public static void SayHello() { 
+	Console.WriteLine("Hello"); //Commit 1 
+
+	int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+	Console.WriteLine("Array initialized."); //Commit 2
+}
